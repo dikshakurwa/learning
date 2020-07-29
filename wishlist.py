a5e0f2f38c366dd0b04733453f9f8d885630380f
dikshakurwa <68566156+dikshakurwa@users.noreply.github.com>
2020-07-29 15:23:01 -0500

    Create wishlist.py

---
+++ wishlist.py
@@ -0,0 +1,6 @@
+wishList = ["takis", "new macbook", "license", "car", "vacation"]
+item = str(input("please enter one of diksha's wishes: "))
+if item in wishList:
+	print("True")
+else:
+	print("False")
