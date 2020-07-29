027316f1620a6beadc83454590ab759949db456e
dikshakurwa <68566156+dikshakurwa@users.noreply.github.com>
2020-07-29 15:21:29 -0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -10,11 +10,11 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+print("Select operation")
+print("1 - add")
+print("2 - subtract")
+print("3 - multiply")
+print("4 - divide")
 
 while True:
     choice = input("Enter choice(1/2/3/4): ")
